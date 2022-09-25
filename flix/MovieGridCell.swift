d75c43fe1dcc61663312bfaa4bb6015ac3538a03
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2022-09-25 13:27:08 -0400

    movie grid details working

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Paige Thompson on 9/24/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
